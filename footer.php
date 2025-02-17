72ec71db5a1ad8e9dfc1d0c0ed2413a0db378385
Wirooo03 <wryd.vac@gmail.com>
2025-02-17 11:20:41 +0700

    push project

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="footer" style="text-align: center; background-color: #2c3e50";>
+    Universitas Muhammadiyah Malang : Informatika 2022 <br>
+    <strong> © Project Caslab 2022 <strong>
+</div>
+
